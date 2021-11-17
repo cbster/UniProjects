f158158944d36223ba6e8e8d4b348ab7445d4d09
Cbster <cbster@protonmail.com>
2021-11-17 08:39:35 +0100

    Bonusaufgabe completed

--- src/Looping.java
+++ src/Looping.java
@@ -1,22 +1,36 @@
+import java.math.BigInteger;
+
 public class Looping {
-    static long reverseNumber(String stringNum) {
+    static long reverseNumber(String stringNum) {  // This function reverses a Long by converting it to a string and back
         String reversedStringNum = new StringBuilder(stringNum).reverse().toString();
-        long reversedLong = Long.parseLong(reversedStringNum);
-        return reversedLong;
+        return Long.parseLong(reversedStringNum);
     }
 
-    static void palCheck(long testNumber, long attemptNo) {
-        if (attemptNo < 100) {
-            long reversedNumber = reverseNumber(Long.toString(testNumber));
-            long result = testNumber + reversedNumber;
-            long reversedResult = reverseNumber(Long.toString(result));
-            if (result == reversedResult) {
-                return;
+    static BigInteger reverseBig(String stringNum) { // As above, returning a BigInteger
+        String reversedStringNum = new StringBuilder(stringNum).reverse().toString();
+        return new BigInteger(reversedStringNum);
+    }
+
+    static void palCheck(long testNumber, long attemptNo) { // Checks long for palindrome
+        long reversedNumber = reverseNumber(Long.toString(testNumber));
+        long result = testNumber + reversedNumber;
+        long reversedResult = reverseNumber(Long.toString(result));
+        if (result != reversedResult) {
+            palCheck(result, ++attemptNo); // Will run until NumberFormatException or until palindrome is found
+        }
+    }
+
+    static void palCheckBig(BigInteger testNumber, long attemptNo, long initNum) { // Checks bigIntegers for palindromes
+        if (attemptNo <= 100) { // Limit attempts to 100 per exercise specification
+            BigInteger reversedNumber = reverseBig(testNumber.toString());
+            BigInteger result = testNumber.add(reversedNumber);
+            BigInteger reversedResult = reverseBig(result.toString());
+            if (result.equals(reversedResult)) {
+                System.out.println(initNum + " braucht " + attemptNo + " Iterationen bis zum Palindrom " + result);
+                System.exit(0);
             } else {
-                palCheck(result, ++attemptNo);
+                palCheckBig(result, ++attemptNo, initNum);
             }
-        } else {
-            return;
         }
     }
 
@@ -28,13 +42,20 @@ public class Looping {
             for (long testNumber = 1; testNumber < Integer.parseInt(args[0]); testNumber++) {
                 try {
                     palCheck(testNumber, 1);
-                }catch (NumberFormatException e){
+                } catch (NumberFormatException e) {
                     System.out.println(testNumber);
                 }
             }
         }
         if (args.length == 2) {
-
+            for (long testNumber = 1; testNumber < Integer.parseInt(args[0]); testNumber++) {
+                try {
+                    palCheck(testNumber, 1);
+                } catch (NumberFormatException e) {
+                    palCheckBig(new BigInteger(String.valueOf(testNumber)), 1, testNumber);
+                    }
+                }
+            System.out.println("alle Zahlen werden auch durch Abbruch per Ueberlauf gefunden");
+            }
         }
     }
-}
