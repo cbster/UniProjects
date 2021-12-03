48553a9321630b1beec29c1639a2efba89c4c9f1
Cbster <cbster@protonmail.com>
2021-12-03 22:02:55 +0100

    Completed Bigs + removed sussyness

--- src/Bigs.java
+++ src/Bigs.java
@@ -1,28 +1,56 @@
 class Bigs {
-
     // addiert die Ziffernfelder a und b
     public static int[ ] add (int[ ] a, int[ ] b) { /* TODO */ }
 
     // gibt das Ziffernfeld n in lesbarer dezimaler Form aus
-    static void print (int[ ] n)             { /* TODO */ }
+    static void print(int[] n) {
+        for (int i = n.length; i >= 0; i--) {
+            System.out.print(n[i]);
+        }
+    }
 
     // konstruiert ein einstelliges Ziffernfeld aus d
-    static int[ ] digit(int d)	             { /* TODO */ }
+    static int[] digit(int d) {
+        int[] a = {d};
+        return a;
+    }
 
     // konstruiert das Ziffernfeld, welches den Wert Null repraesentiert
-    static int[ ] Null()                     { /* TODO */ }
+    static int[] Null() {
+        int[] a = {0};
+        return a;
+    }
 
     // konstruiert das Ziffernfeld, welches den Wert Eins repraesentiert
-    static int[ ] One()                     { /* TODO */ }
+    static int[] One() {
+        int[] a = {1};
+        return a;
+    }
 
     // Rest des Ziffernfeldes n bei Division durch 10 (eine int-Zahl!)
-    static int mod10(int[ ] n)               { /* TODO */ }
+    static int mod10(int[] n) {
+        return n[0];
+    }
 
     // ganzzahliger Quotient bei Division durch 10
-    static int[ ] div10(int[ ] n)            { /* TODO */ }
+    static int[] div10(int[] n) {
+        int[] quotArr = new int[n.length - 1];
+        for (int i = 0; i < n.length - 1; i++) {
+            quotArr[i] = n[i];
+        }
+        return quotArr;
+    }
 
     // Umwandlung einer int-Zahl in ein Ziffernfeld
-    static int[ ] fromInt(int n)             { /* TODO */ }
+    static int[] fromInt(int n) {
+        String stringInt = Integer.toString(n);
+        char[] strArray = stringInt.toCharArray();
+        int[] nums = new int[strArray.length];
+        for (int i = strArray.length - 1; i >= 0; i--) {
+            nums[(strArray.length - 1 - i)] = Integer.parseInt(Character.toString(strArray[i]));
+        }
+        return nums;
+    }
 
     // kopiert den Wert von a
     static int[ ] copy(int[ ] n)             { /* TODO */ }
@@ -31,35 +59,116 @@ class Bigs {
     static int[ ] times(int[ ] n, int d)     { /* TODO */ }
 
     // multipliziert das Ziffernfeld n mit 10
-    static int[ ] times10(int[ ] n)          { /* TODO */ }
+    static int[] times10(int[] n) {
+        int[] tenProduct = new int[n.length + 1];
+        for (int i = 0; i < n.length; i++) {
+            tenProduct[i + 1] = n[i];
+        }
+        tenProduct [0] = 0;
+        return tenProduct;
+    }
 
     // multipliziert zwei Ziffernfelder
-    static int[ ] times(int[ ]a, int[ ] b)   { /* TODO */ }
+    static int[] times(int[] a, int[] b) {
+        int[] product = new int[a.length + b.length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                product[i + j] += a[i] * b[j];
+            }
+        }
+        for (int i = 0; i < product.length - 1; i++) {
+            while (product[i] > 9) {
+                product[i] = product[i] - 10;
+                product[i + 1]++;
+            }
+        }
+        if (ok(product)) {
+            return product;
+        } else {
+            int[] newProduct = new int[product.length - 1];
+            for (int i = 0; i < product.length - 1; i++) {
+                newProduct[i] = product[i];
+            } return newProduct;
+        }
+    }
 
     // Quadratzahl eines Ziffernfeldes
-    static int[ ] square(int[ ]a)            { /* TODO */ }
+    static int[] square(int[] a) {
+        return times(a, a);
+    }
 
     // Kubikzahl eines Ziffernfeldes
-    static int[ ] cubic(int[ ]a)             { /* TODO */ }
+    static int[] cubic(int[] a) {
+        return times(times(a, a), a);
+    }
 
     // Test auf kleiner-Relation zweier Ziffernfelder: a < b ?
-    static boolean less (int[ ] a, int[ ] b) { /* TODO */ }
+    static boolean less(int[] a, int[] b) {
+        if (a.length < b.length) {
+            return true;
+        }
+        for (int i = 1; i <= a.length; i++) {
+            if (a[a.length - i] < b[b.length - i]) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     // Test auf Gleichheit zweier Ziffernfelder
-    static boolean equal (int[ ] a, int[ ] b){ /* TODO */ }
+    static boolean equal(int[] a, int[] b) {
+        if (a.length != b.length) {
+            return false;
+        }
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     // Test auf Korrektheit eines Ziffernfeldes: Feld existiert und enthaelt
     // mindestens eine Ziffer, alle Positionen liegen zwischen 0 und 9
     // keine fuehrenden Nullen (ausser bei Null selbst)
-    static boolean ok (int[ ] n)             { /* TODO */ }
+    static boolean ok(int[] n) {
+        if (n.length == 0) {
+            return false;
+        }
+        for (int num: n) {
+            if (num > 9) {
+                return false;
+            }
+        }
+        if ((n[n.length-1] < 1) && (n.length != 1)) {
+            return false;
+        }
+        return true;
+    }
 
     // gibt die (kleinste) Ziffer mit der groessten Haeufigkeit in n aus
-    static void maxDigit(int[] n)            { /* TODO */ }
+    static void maxDigit(int[] n) {
+        int[] counter = new int[10];
+        for (int j : n) {
+            counter[j]++;
+        }
+        int maxPos = 0;
+        int maxVal = 0;
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i] > maxVal) {
+                maxPos = i;
+                maxVal = counter[i];
+            }
+        }
+        System.out.println(maxPos);
+    }
 
-    public static void main (String[ ] s) {
+
+    public static void main(String[] s) {
         int[] a = One();
+        fromInt(123);
 
-        for (int i=0; i<33222; ++i) {
+        for (int i = 0; i < 33222; ++i) {
             a = times(a, 2);
         }
 
@@ -70,8 +179,8 @@ class Bigs {
         int[] b = fromInt(13);
         int[] c = copy(b);
 
-        for (int i=1; i<8978; ++i) {
-            c=times(c, b);
+        for (int i = 1; i < 8978; ++i) {
+            c = times(c, b);
         }
 
         System.out.println("13^8978 hat " + c.length + " Stellen");
