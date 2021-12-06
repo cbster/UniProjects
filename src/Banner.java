29db4c1c643de30bd8ca07d7c1a64618ac239b14
Cbster <cbster@protonmail.com>
2021-12-06 13:42:26 +0100

    Minor change to comment

--- src/Banner.java
+++ src/Banner.java
@@ -84,7 +84,7 @@ public class Banner {
     public static void main(String[] args) {
         String[] lines = {"","","","","","",""}; // Initialise 7 empty lines
         for (char ch : args[0].toCharArray()) { // For each digit/character in the number
-            int mapTo = (ch == 45)? ch - 35 : ch - 48; // Ternary check maps integer or - to matching sign in fontChars
+            int mapTo = (ch == 45)? ch - 35 : ch - 48; // Ternary check maps integer or "-" to matching sign in fontChars
             for (int i = 0; i<7; i++) { // For all 7 lines
                 lines[i] = lines[i] + fontChars[mapTo][i]; // Append the matching fontChars line to each line
             }
