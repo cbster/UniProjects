f25135f1c0ff9863759433dcff2c9d2d5c57cf43
Cbster <cbster@protonmail.com>
2021-12-06 13:42:45 +0100

    Comment code

--- src/Deduplikator.java
+++ src/Deduplikator.java
@@ -2,17 +2,17 @@ public class Deduplikator {
     public static void main(String[] args) {
         double[] nums = new double[args.length];
         for (int i = 0; i < args.length; i++) {
-            nums[i] = (Double.parseDouble(args[i]));
+            nums[i] = (Double.parseDouble(args[i])); // Parses all arguments to Double
         }
-        for (int i = 0; i < nums.length; i++) {
-            boolean found = false;
-            for (int j = 0; j < i; j++) {
-                if (nums[i] == nums[j]) {
+        for (int i = 0; i < nums.length; i++) { // Checking each number in nums
+            boolean found = false; // By default, number is not found as duplicate
+            for (int j = 0; j < i; j++) { // Check each number up until i
+                if (nums[i] == nums[j]) { // If the numbers at i and j are the same, duplicate found
                     found = true;
                     break;
                 }
             }
-            if (!found) {
+            if (!found) { // if no duplicate was found, print number
                 System.out.println(nums[i]);
             }
         }
